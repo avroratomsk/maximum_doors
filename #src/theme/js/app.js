99586f280584c2580eaad1f6eff6fdc1591b4781
sanchyopas <saniagolovanev@gmail.com>
2024-11-27 18:06:51 +0700

    first commit

--- #src/theme/js/app.js
+++ #src/theme/js/app.js
@@ -3,3 +3,17 @@ import * as functions from "./modules/functions.js";
 functions.isWebp();
 
 import '../js/modules/menu/stickyMenu.js'
+
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+const swiper = new Swiper('.index-reviews__slider', {
+  modules: [Navigation, Pagination],
+  slidesPerView: 4.5,
+  spaceBetween: 30,
+});
+const swiper_news = new Swiper('.index-news__slider', {
+  modules: [Navigation, Pagination],
+  slidesPerView: 3,
+  spaceBetween: 30,
+});
