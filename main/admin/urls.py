f634aa2bca9788d6502856b6de62f1b76b8afe64
sanchyopas <saniagolovanev@gmail.com>
2024-12-19 12:48:41 +0700

    dev: page

--- main/admin/urls.py
+++ main/admin/urls.py
@@ -18,9 +18,7 @@ urlpatterns = [
     path('category/add/', views.category_add, name='category_add'),
     path('category/edit/<int:pk>/', views.category_edit, name='category_edit'),
     path('category/delete/<int:pk>/', views.category_delete, name='category_delete'),
-    
-    
-    
+
     #URl - отвечающие за отображение дня недели, редактирование и удаление дня недели
     path('days/', views.day_product, name='admin_day'),
     path('days/add/', views.day_add, name='days_add'),
--- main/service/urls.py
+++ main/service/urls.py
@@ -5,5 +5,7 @@ from service import views
 
 urlpatterns = [
     path('', views.service, name="service"),
-    # path('<slug:slug>/', views.service_detail, name="service_detail"),
+    path('<slug:slug>', views.service_detail, name="service_detail"),
+    path('okna/', views.service_detail, name="service_detail"),
+    path('/lodjii/', views.service_detail, name="service_detail"),
 ]
