85272228a9ffac268a23cf16d53c58b140077aaa
sanchyopas <saniagolovanev@gmail.com>
2025-05-31 17:49:23 +0700

    fix cookies

--- #src/theme/js/app.js
+++ #src/theme/js/app.js
@@ -115,17 +115,19 @@ function formatPhoneNumber(phoneNumber) {
   return "+7" + cleanedNumber;
 }
 
-// Проверяем, установлены ли куки
-if (!document.cookie.split('; ').find(row => row.startsWith('cookie_consent='))) {
-  // Если куки не установлены, показываем уведомление
-  document.getElementById('cookie-notice').style.display = 'block';
-}
+document.addEventListener('DOMContentLoaded', () => {
+  // Проверяем, установлены ли куки
+  if (!document.cookie.split('; ').find(row => row.startsWith('cookie_consent='))) {
+    // Если куки не установлены, показываем уведомление
+    document.getElementById('cookie-notice').style.display = 'block';
+  }
 
 // Обработчик для кнопки согласия
-document.getElementById('accept-cookies').addEventListener('click', function() {
-  console.log('click')
-  // Устанавливаем куки на 1 год
-  document.cookie = "cookie_consent=true; max-age=" + 60 * 60 * 24 * 365 + "; path=/";
-  // Скрываем уведомление
-  document.getElementById('cookie-notice').style.display = 'none';
-});
+  document.getElementById('accept-cookies').addEventListener('click', function() {
+    console.log('click')
+    // Устанавливаем куки на 1 год
+    document.cookie = "cookie_consent=true; max-age=" + 60 * 60 * 24 * 365 + "; path=/";
+    // Скрываем уведомление
+    document.getElementById('cookie-notice').style.display = 'none';
+  });
+})
