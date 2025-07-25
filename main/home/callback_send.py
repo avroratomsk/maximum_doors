2a19b6e3d449e5220636f1ab7c0c452073c507c4
sanchyopas <saniagolovanev@gmail.com>
2025-07-25 17:29:39 +0700

    fix

--- main/home/callback_send.py
+++ main/home/callback_send.py
@@ -6,10 +6,8 @@ EMAIL_FROM = "info@xn----7sbah6bllcobpj.xn--p1ai"
 
 try:
   email_clients = BaseSettings.objects.get().email
-  print(email_clients)
 except:
   email_clients = 'saniagolovanev@gmail.com'
-  print(email_clients)
   
 
 def email_callback(messages, title):
